1ea2c5dd2d96419efe5c1c7ab751ba9db374aeaa
Selva Nair <selva.nair@gmail.com>
2018-03-25 18:12:18 -0400

    Refactor multi_add_iroutes
    Move out the code for adding single iroutes from multi_add_iroutes
    into separate functions for both IPv4 and IPv6.
    
    No functionality changes. This refactoring helps the proposed
    patch for adding iroutes through the management.
    
    Signed-off-by: Selva Nair <selva.nair@gmail.com>

--- src/openvpn/multi.c
+++ src/openvpn/multi.c
@@ -1304,6 +1304,51 @@ multi_learn_in6_addr(struct multi_context *m,
     }
 }
 
+/* helper to add an entry to the internal ipv4 routing table */
+static void
+multi_add_iroute_entry(struct multi_context *m, struct multi_instance *mi,
+                       const struct iroute *ir)
+{
+    struct gc_arena gc = gc_new();
+    if (ir->netbits >= 0)
+    {
+        msg(D_MULTI_LOW, "MULTI: internal route %s/%d -> %s",
+            print_in_addr_t(ir->network, 0, &gc),
+            ir->netbits,
+            multi_instance_string(mi, false, &gc));
+    }
+    else
+    {
+        msg(D_MULTI_LOW, "MULTI: internal route %s -> %s",
+            print_in_addr_t(ir->network, 0, &gc),
+            multi_instance_string(mi, false, &gc));
+    }
+
+    mroute_helper_add_iroute46(m->route_helper, ir->netbits);
+
+    multi_learn_in_addr_t(m, mi, ir->network, ir->netbits, false);
+
+    gc_free(&gc);
+}
+
+/* helper to add an entry to the internal ipv6 routing table */
+static void
+multi_add_iroute6_entry(struct multi_context *m, struct multi_instance *mi,
+                       const struct iroute_ipv6 *ir6)
+{
+    struct gc_arena gc = gc_new();
+    msg(D_MULTI_LOW, "MULTI: internal route %s/%d -> %s",
+        print_in6_addr(ir6->network, 0, &gc),
+        ir6->netbits,
+        multi_instance_string(mi, false, &gc));
+
+    mroute_helper_add_iroute46(m->route_helper, ir6->netbits);
+
+    multi_learn_in6_addr(m, mi, ir6->network, ir6->netbits, false);
+
+    gc_free(&gc);
+}
+
 /*
  * A new client has connected, add routes (server -> client)
  * to internal routing table.
@@ -1312,7 +1357,6 @@ static void
 multi_add_iroutes(struct multi_context *m,
                   struct multi_instance *mi)
 {
-    struct gc_arena gc = gc_new();
     const struct iroute *ir;
     const struct iroute_ipv6 *ir6;
     if (TUNNEL_TYPE(mi->context.c1.tuntap) == DEV_TYPE_TUN)
@@ -1320,37 +1364,13 @@ multi_add_iroutes(struct multi_context *m,
         mi->did_iroutes = true;
         for (ir = mi->context.options.iroutes; ir != NULL; ir = ir->next)
         {
-            if (ir->netbits >= 0)
-            {
-                msg(D_MULTI_LOW, "MULTI: internal route %s/%d -> %s",
-                    print_in_addr_t(ir->network, 0, &gc),
-                    ir->netbits,
-                    multi_instance_string(mi, false, &gc));
-            }
-            else
-            {
-                msg(D_MULTI_LOW, "MULTI: internal route %s -> %s",
-                    print_in_addr_t(ir->network, 0, &gc),
-                    multi_instance_string(mi, false, &gc));
-            }
-
-            mroute_helper_add_iroute46(m->route_helper, ir->netbits);
-
-            multi_learn_in_addr_t(m, mi, ir->network, ir->netbits, false);
+            multi_add_iroute_entry(m, mi, ir);
         }
         for (ir6 = mi->context.options.iroutes_ipv6; ir6 != NULL; ir6 = ir6->next)
         {
-            msg(D_MULTI_LOW, "MULTI: internal route %s/%d -> %s",
-                print_in6_addr(ir6->network, 0, &gc),
-                ir6->netbits,
-                multi_instance_string(mi, false, &gc));
-
-            mroute_helper_add_iroute46(m->route_helper, ir6->netbits);
-
-            multi_learn_in6_addr(m, mi, ir6->network, ir6->netbits, false);
+            multi_add_iroute6_entry(m, mi, ir6);
         }
     }
-    gc_free(&gc);
 }
 
 /*
